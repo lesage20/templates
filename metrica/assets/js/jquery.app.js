b66b788e50c122d48a5771c91978a2bb945970e5
lesage20 <angezanou00@gmail.com>
2021-07-23 20:17:15 +0000

    Initial commit

---
+++ metrica/assets/js/jquery.app.js
@@ -0,0 +1,63 @@
+/* Theme Name: Metrico - Responsive Landing page template
+   Author: Coderthemes
+   Author e-mail: coderthemes@gmail.com
+   Version: 1.0.0
+   Created:March 2016
+   File Description:Main JS file of the template
+*/
+
+
+(function ($) {
+
+    'use strict';
+
+    function initNavbar() {
+
+        var scroll = $(window).scrollTop();
+
+        $('.navbar-toggle').on('click', function (event) {
+            $(this).toggleClass('open');
+            $('#navigation').slideToggle(400);
+        });
+
+        $('.navigation-menu>li').slice(-2).addClass('last-elements');
+
+        $('.menu-arrow,.submenu-arrow').on('click', function (e) {
+            if ($(window).width() < 992) {
+                e.preventDefault();
+                $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
+            }
+        });
+    }
+
+    function initSearch() {
+        $('.toggle-search').on('click', function () {
+            var targetId = $(this).data('target');
+            var $searchBar;
+            if (targetId) {
+                $searchBar = $(targetId);
+                $searchBar.toggleClass('open');
+            }
+        });
+    }
+
+    function initNavitemActive() {
+        $(".navigation-menu a").each(function () {
+            if (this.href == window.location.href) {
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().parent().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().parent().parent().addClass("active"); // add active class to an anchor
+            }
+        });
+    }
+
+
+    function init() {
+        initNavbar();
+        initSearch();
+        initNavitemActive();
+    }
+
+    init();
+
+})(jQuery)
